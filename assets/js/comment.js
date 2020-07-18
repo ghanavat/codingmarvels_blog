fdc8a8f6f99aae27426b1376658b8ca1fabcd571
Saeed Ghanavat <dev_projects@ghanavat.co.uk>
2020-07-18 22:01:20 +0100

    comment.js updated to intialise without document.ready

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -1,84 +1,60 @@
-$(document).ready(function(){
-    var controls = {
-      email: null,
-      status: null,
-      submitButton: null,
-      fields: null,
-      avatarPreview: null
-    };
+var emailRegEx = /[^@\s]+@[^@\s]+\.[^@\s]+$/
 
-    var globalVariables = {
-      emailRegEx: /[^@\s]+@[^@\s]+\.[^@\s]+$/
-    };
-    
-    controls.email = $('#email');
-    controls.status = document.getElementById('commentstatus');
-    controls.status.innerText = '';
+function changedAvatar() {
+  var image = document.getElementById('avatarPreview')
+  image.default = image.default || image.src
+  image.possible = buildPossibleAvatars(document.getElementById('identity').value);
+  image.currentIndex = 0;
+  setAvatar(image);
+}
 
-    controls.submitButton = document.getElementById('commentbutton');
-    controls.submitButton.onclick = (e) => { validateFields() }
+function buildPossibleAvatars(identity) {
+    var possibleAvatars = []
+  
+    if (identity.match(emailRegEx))
+      possibleAvatars.push('https://secure.gravatar.com/avatar/' + md5(identity) + '?s=80&d=identicon&r=pg')
+  
+    if (identity.indexOf('@') < 0)
+      possibleAvatars.push('https://github.com/' + identity + '.png')
+  
+    possibleAvatars.push('https://avatars.io/twitter/' + identity + '/medium')
+  
+    return possibleAvatars
+  }
 
-    controls.fields = document.getElementById('commentfields');
+function setAvatar(image, increment) {
+    if (increment)
+      image.currentIndex += increment
+  
+    image.src = image.currentIndex < image.possible.length
+      ? image.possible[image.currentIndex]
+      : image.default
+  
+    document.getElementById('avatar').value = image.src
+}
 
-    controls.avatarPreview = document.getElementById('avatarPreview');
-    controls.avatarPreview.onerror = (e) => { setAvatar(e.target, 1) }
+function prepareCommitForm() {
+  var status = document.getElementById('commentstatus')
+  status.innerText = ''
 
-    function changedAvatar() {
-      let image = controls.avatarPreview;
-      image.default = image.default || image.src
-      image.possible = buildPossibleAvatars(document.getElementById('identity').value);
-      image.currentIndex = 0;
-      setAvatar(image);
-    }
+  var requiredIds = [ 'message', 'identity', 'name']
+  var missing = requiredIds.filter(id => document.getElementById(id).value.length < 3)
+  if (missing.length > 0) {
+    status.innerText = 'Some required fields are missing - (' + missing.join(', ') + ')'
+    return
+  }
 
-    function buildPossibleAvatars(identity) {
-        var possibleAvatars = []
-      
-        if (identity.match(globalVariables.emailRegEx))
-          possibleAvatars.push('https://secure.gravatar.com/avatar/' + md5(identity) + '?s=80&d=identicon&r=pg')
-      
-        if (identity.indexOf('@') < 0)
-          possibleAvatars.push('https://github.com/' + identity + '.png')
-      
-        possibleAvatars.push('https://avatars.io/twitter/' + identity + '/medium')
-      
-        return possibleAvatars
-      }
+  var button = document.getElementById('commentbutton')
+  if (button.innerText != 'Confirm comment') {
+    button.innerText = 'Confirm comment'
+    return
+  }
 
-    function setAvatar(image, increment) {
-        if (increment)
-          image.currentIndex += increment
-      
-        image.src = image.currentIndex < image.possible.length
-          ? image.possible[image.currentIndex]
-          : image.default
-      
-        document.getElementById('avatar').value = image.src
-      }
-
-    function validateFields(){
-        var requiredIds = [ 'message', 'name', 'identity'];
-        var missing = requiredIds.filter(id => document.getElementById(id).value.length < 3);
-        if (missing.length > 0) {
-        controls.status.innerText = 'Some required fields are missing - (' + missing.join(', ') + ')';    
-        return;
-        }
-
-        if (controls.submitButton.innerText != 'Confirm comment') {
-        controls.submitButton.innerText = 'Confirm comment';
-        return;
-        }
-
-        var form = document.getElementById('commentform');
-        form.action = '{{ site.comments.receiver }}';
-        controls.submitButton.innerText = 'Posting...';
-        controls.submitButton.disabled = true;
-        form.submit();
-        
-        controls.fields.disabled = true;
-    }    
-
-    document.getElementById('identity').onchange = () => {
-      changedAvatar()
-    }    
-  })
+  var form = document.getElementById('commentform')
+  form.action = form.getAttribute('action2')
+  button.innerText = 'Posting...'
+  button.disabled = true
+  form.submit()
+  var fields = document.getElementById('commentfields')
+  fields.disabled = true
+}
