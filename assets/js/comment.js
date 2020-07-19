e460757b9fdaf4f47fa5e815e990ce701d829640
Saeed Ghanavat <dev_projects@ghanavat.co.uk>
2020-07-19 02:41:51 +0100

    Added validation function. Fixed the style for the validation message Added 'name' attribute for all required fields Status div is now hidden by default

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -1,60 +1,77 @@
-var emailRegEx = /[^@\s]+@[^@\s]+\.[^@\s]+$/
+var emailRegEx = /[^@\s]+@[^@\s]+\.[^@\s]+$/;
 
 function changedAvatar() {
-  var image = document.getElementById('avatarPreview')
-  image.default = image.default || image.src
+  var image = document.getElementById('avatarPreview');
+  image.default = image.default || image.src;
   image.possible = buildPossibleAvatars(document.getElementById('identity').value);
   image.currentIndex = 0;
   setAvatar(image);
 }
 
 function buildPossibleAvatars(identity) {
-    var possibleAvatars = []
+    var possibleAvatars = [];
   
-    if (identity.match(emailRegEx))
-      possibleAvatars.push('https://secure.gravatar.com/avatar/' + md5(identity) + '?s=80&d=identicon&r=pg')
+    if (identity.match(emailRegEx)){
+      possibleAvatars.push('https://secure.gravatar.com/avatar/' + md5(identity) + '?s=80&d=identicon&r=pg');
+    }
   
-    if (identity.indexOf('@') < 0)
-      possibleAvatars.push('https://github.com/' + identity + '.png')
+    if (identity.indexOf('@') < 0){
+      possibleAvatars.push('https://github.com/' + identity + '.png');
+    }      
   
-    possibleAvatars.push('https://secure.gravatar.com/avatar/twitter/' + identity + '/medium')
+    possibleAvatars.push('https://avatars.io/twitter/' + identity + '/medium');
   
-    return possibleAvatars
-  }
+    return possibleAvatars;
+}
 
 function setAvatar(image, increment) {
-    if (increment)
-      image.currentIndex += increment
+    if (increment){
+      image.currentIndex += increment;
+    }
   
     image.src = image.currentIndex < image.possible.length
       ? image.possible[image.currentIndex]
-      : image.default
+      : image.default;
   
-    document.getElementById('avatar').value = image.src
+    document.getElementById('avatar').value = image.src;
 }
 
-function prepareCommitForm() {
-  var status = document.getElementById('commentstatus')
-  status.innerText = ''
-
-  var requiredIds = [ 'message', 'identity', 'name']
-  var missing = requiredIds.filter(id => document.getElementById(id).value.length < 3)
-  if (missing.length > 0) {
-    status.innerText = 'Some required fields are missing - (' + missing.join(', ') + ')'
-    return
+function ConfirmComment() {
+  if (!validateFields()) {
+    return;
   }
 
-  var button = document.getElementById('commentbutton')
+  var button = document.getElementById('commentbutton');
   if (button.innerText != 'Confirm comment') {
-    button.innerText = 'Confirm comment'
-    return
+    button.innerText = 'Confirm comment';
+    return;
   }
 
-  var form = document.getElementById('commentform')
-  form.action = form.getAttribute('action2')
-  button.innerText = 'Posting...'
-  button.disabled = true
-  form.submit()
-  var fields = document.getElementById('commentfields')
-  fields.disabled = true
+  var form = document.getElementById('commentform');
+  form.action = form.getAttribute('action2');
+  button.innerText = 'Posting...';
+  button.disabled = true;
+  form.submit();
+  var fields = document.getElementById('commentfields');
+  fields.disabled = true;
+}
+
+function validateFields(){
+  var status = document.getElementById('commentstatus');
+  status.innerText = '';
+  var invalidFields = [];
+  
+  var requiredFields = document.getElementById('commentfields').querySelectorAll('[required]');
+
+  for (let index = 0; index < requiredFields.length; index++) {    
+    if (requiredFields[index].value.length == 0) {
+      invalidFields.push(requiredFields[index].name);
+    }
+  }
+
+  if (invalidFields.length > 0) {
+      status.classList.remove('hidden');
+      status.innerText = 'Some required fields are missing - (' + invalidFields.join(', ') + ')';
+      return false;
+  }
 }
